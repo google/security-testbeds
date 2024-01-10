84e9b6d4cc174649405859c157a9c5ce24498151
João Aguinsky <aguinsky@google.com>
2024-01-10 19:29:24 +0000

    Fix challenge
    now possible to connect through nsjail

--- jupyter/chal-jupyter/challenge/start.sh
+++ jupyter/chal-jupyter/challenge/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-/usr/local/bin/jupyter-notebook
+# Start node web server
+(&>/dev/null /usr/local/bin/jupyter-notebook)&
 
-#socat - TCP:127.0.0.1:8888,forever
+# Proxy stdin/stdout to web server
+socat - TCP:127.0.0.1:8888,forever
