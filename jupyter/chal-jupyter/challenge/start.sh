45a96a7320d7fe2782492ae061ec942483fd0fb4
João Pedro Aguinsky <aguinsky@google.com>
2024-02-01 18:06:24 +0000

    Adds support for flag secret in exposed ui

--- jupyter/chal-jupyter/challenge/start.sh
+++ jupyter/chal-jupyter/challenge/start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# Start node web server
-(&>/dev/null /usr/local/bin/jupyter-notebook)&
+#puts flag in html page if it is not there
+FLAG=$( < /flag/flag)
+if ! grep -q $FLAG /usr/local/lib/python3.8/dist-packages/jupyter_server/templates/page.html; then
+	sed -i 26r/flag/flag /usr/local/lib/python3.8/dist-packages/jupyter_server/templates/page.html;
+fi
+unset flag
 
-# Proxy stdin/stdout to web server
+
+# Start node web server
+/usr/local/bin/jupyter-notebook > /dev/null 2>&1 &
+
+
+# Proxy stdin/stdout to server
 socat - TCP:127.0.0.1:8888,forever
