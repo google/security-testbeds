fe5ae3b1ee1d1c33096fc1198473bbda88cc4206
SandBox <86597176+secureness@users.noreply.github.com>
2024-04-04 03:13:07 +0330

    Create model.py

---
+++ triton/triton-inference-server/SimpleModel/1/model.py
@@ -0,0 +1,13 @@
+
+import os
+
+class TritonPythonModel:
+
+    def initialize(self, args):
+        print("heyyyyyyyyyyyyyyyyyyyyyyy")
+
+    def execute(self, requests):
+        return
+
+    def finalize(self):
+        return
