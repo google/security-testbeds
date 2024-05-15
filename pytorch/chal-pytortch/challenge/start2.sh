946c791aa09c99c107d593c32c5a11fe26326c3e
jp-ag <jpaguinsky@gmail.com>
2024-05-15 14:54:27 +0000

    Adding kCTF challenge for pytorch

---
+++ pytorch/chal-pytortch/challenge/start2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "$ROOTFLAG" > /flag/flag.txt
+
+cp /flag.mar /home/model-server/model-store/"$AUTHFLAG".mar
+
+/home/venv/bin/torchserve --start --plugins-path /home/model-server/jars --ts-config /home/model-server/config.properties --models "$AUTHFLAG".mar > /home/model-server/logs/log &
+socat - TCP:127.0.0.1:8080,forever
