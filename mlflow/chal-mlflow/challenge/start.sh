78f0e3616c44fae0f05d0a994c7fd00cea67a17a
AndrzejChmielewski <89131357+AndrzejChmielewski@users.noreply.github.com>
2024-06-27 12:26:41 +0200

    updated mlflow

--- mlflow/chal-mlflow/challenge/start.sh
+++ mlflow/chal-mlflow/challenge/start.sh
@@ -4,5 +4,5 @@ cp /basic_auth.db /home/user
 
 sqlite3 /home/user/basic_auth.db "UPDATE users SET password_hash = '$PASSWORD' WHERE id = 1;"
 
-(/usr/local/bin/mlflow server --host 0.0.0.0 --dev --app-name basic-auth --workers=1) >> /logs/log 2>&1 &
+(/usr/local/bin/mlflow server --host 0.0.0.0 --dev --app-name basic-auth --workers=1) >> /logs/$(date +%s).log  2>&1 &
 socat - TCP:127.0.0.1:5000,forever 
