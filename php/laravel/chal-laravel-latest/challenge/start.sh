c0bf5461ef4743223e819881a85d727d9fcd31dc
AndrzejChmielewski <89131357+AndrzejChmielewski@users.noreply.github.com>
2024-06-27 12:25:18 +0200

    Updated LaravelLatest
    Made to public

--- php/laravel/chal-laravel-latest/challenge/start.sh
+++ php/laravel/chal-laravel-latest/challenge/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/usr/local/bin/php /var/www/html/laravel/laravel-app/artisan serve --host=0.0.0.0 > /logs/log 2>&1 &
-/usr/local/bin/php /var/www/html/laravel/laravel-app/artisan pail -vv > /logs/log 2>&1 &
+/usr/local/bin/php /var/www/html/laravel/laravel-app/artisan serve --host=0.0.0.0 > /logs/$(date +%s).log 2>&1 &
+/usr/local/bin/php /var/www/html/laravel/laravel-app/artisan pail -vv > /logs/$(date +%s).log 2>&1 &
 socat - TCP:127.0.0.1:8000,forever
