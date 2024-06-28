658cea9f3a1fb59a75c2f56ca71537a880fd6714
jp-ag <jpaguinsky@gmail.com>
2024-06-28 20:42:57 +0000

    Changing how password is loaded. Also made /home/user non-tmpfs for consistency

--- jupyter/chal-jupyter/challenge/start.sh
+++ jupyter/chal-jupyter/challenge/start.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # Loads password into jupyter's pass file
-mkdir /home/user/.jupyter
+mkdir -p /home/user/.jupyter
 
+if [ ! -f /home/user/.jupyter/jupyter_server_config.json ]; then
 echo '{
   "IdentityProvider": {
     "hashed_password": "CODE"
   }
-}' | sed "s/CODE/$PASS/g" > /home/user/.jupyter/jupyter_server_config.json
+} ' | sed "s@CODE@$PASS@g" > /home/user/.jupyter/jupyter_server_config.json
 
 
 touch /home/user/$AUTHFLAG
 
 echo $ROOTFLAG > /flag/flag.txt
 
+
 unset ROOTFLAG
 unset AUTHFLAG
 unset PASS
+fi
 
 # Start web server
 /usr/local/bin/jupyter-notebook --ServerApp.allow_remote_access=1 > /logs/$(date +%s).log 2>&1 &
