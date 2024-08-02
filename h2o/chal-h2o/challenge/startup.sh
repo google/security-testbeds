4d612f9605e86eef7ff9affff327ef4196af54cd
jp-ag <jpaguinsky@gmail.com>
2024-08-02 09:06:20 +0000

    Updates logging and secrets to latest. Adds metadata description

--- h2o/chal-h2o/challenge/startup.sh
+++ h2o/chal-h2o/challenge/startup.sh
@@ -2,7 +2,7 @@
 
 echo $ROOTFLAG > /flag/flag.txt 
 
-java -jar /h2o/h2o.jar -hash_login -login_conf /h2o/realm.properties  > /logs/log 2>&1 &
+java -jar /h2o/h2o.jar -hash_login -login_conf /h2o/realm.properties  > /logs/$(date +%s).log 2>&1 &
 
 # Proxy stdin/stdout to server
 socat - TCP:127.0.0.1:54321,forever
