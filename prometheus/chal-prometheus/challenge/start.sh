e003af110058cb5b8775af765a2be4df7195dfb3
jp-ag <jpaguinsky@gmail.com>
2024-10-17 13:31:13 +0000

    Adding kCTF challenge for Prometheus dashboard

---
+++ prometheus/chal-prometheus/challenge/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/home/ubuntu/prometheus/prometheus --config.file=/home/ubuntu/prometheus/prometheus.yml --storage.tsdb.path /home/ubuntu/data > /logs/$(date +%s).log 2>&1 &  
+
+socat - TCP:127.0.0.1:9090,forever
