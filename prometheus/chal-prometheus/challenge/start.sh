e2fa57c741ee491e7198c13c9f35ae2e996982d9
jp-ag <jpaguinsky@gmail.com>
2024-10-18 11:01:47 +0000

    Now prometheus consumes data from node_exporter

--- prometheus/chal-prometheus/challenge/start.sh
+++ prometheus/chal-prometheus/challenge/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+/home/ubuntu/node_exporter/node_exporter > /logs/$(date +%s).log 2>&1 &
 /home/ubuntu/prometheus/prometheus --config.file=/home/ubuntu/prometheus/prometheus.yml --storage.tsdb.path /home/ubuntu/data > /logs/$(date +%s).log 2>&1 &  
 
 socat - TCP:127.0.0.1:9090,forever
+
