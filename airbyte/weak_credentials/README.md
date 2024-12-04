9073d6fd8c9ef8c9cee86d522e7491a1a044e196
secureness <nosecureness@gmail.com>
2024-12-04 19:03:50 +0100

    use an automatic bash script setup

--- airbyte/weak_credentials/README.md
+++ airbyte/weak_credentials/README.md
@@ -1,35 +1,16 @@
 # setup requirements
-I used ubuntu 22.10 as my OS.
-1. Install docker.
-2. k8s with minikube: https://minikube.sigs.k8s.io/docs/start/ (we can use original k8s but this is an easy solution)
-3. please don't forget to add `alias kubectl="minikube kubectl --"` to your shell environment.
-4. download the `abctl` to manage the k8s easier. choose a compatible processor architecture from the release page: https://github.com/airbytehq/abctl/releases/tag/v0.16.0 
-   ```bash
-   # linux amd64
-    curl https://github.com/airbytehq/abctl/releases/download/v0.16.0/abctl-v0.16.0-linux-amd64.tar.gz
-    tar -xvzf abctl-v0.16.0-linux-amd64.tar.gz
-    mv abctl-v0.16.0-linux-amd64/abctl .
-    rm -rf abctl-v0.16.0-linux-amd64
-    chmod +x abctl
-    ```
-Thanks to the James Fox(@jamesfoxxx) for the minikube setup guide.
+Please use the Ubuntu 22.10 as OS.
+
 
 # vulnerable instance
 ```bash
-# clear the previous installation
-./abctl local uninstall
-./abctl local install
-./abctl local credentials --email user@company.example
-./abctl local credentials --password new_password
+bash vulnerableAirbyte.sh
 ```
-Go to http://localhost:8000/ and enter airbyte/password as username and password.
 
 # secure instance
 ```bash
-# clear the previous installation
-./abctl local uninstall
-./abctl local install
-# get the random username and password
-./abctl local credentials
+
+bash secureAirbyte.sh
 ```
-Choose an email as a username and enter the random password that you received in a terminal. Since the password is random this is a secure airbyte instance. 
+
+Thanks to the James Fox(@jamesfoxxx) for the minikube setup guide.
---
+++ airbyte/weak_credentials/vulnerableAirbyte.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -e
+
+echo "An Automated Bash script for setting up a vulnerable airbyte instance that contains default login credentials"
+
+echo "Updating package index..."
+sudo apt update
+
+echo  "Add Docker's official GPG key..."
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo  "Add the repository to Apt sources..."
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+echo "Installing Docker..."
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+docker info || (echo "${RED}docker is not running" ; exit 1)
+
+echo "Adding current user to the 'docker' group..."
+sudo usermod -aG docker "$USER"
+
+echo "Installing Minikube..."
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 || (echo "${RED}can't download minikube" ; exit 1)
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+rm minikube-linux-amd64
+
+echo "Starting Minikube..."
+minikube start
+
+echo "Adding alias for kubectl..."
+echo 'alias kubectl="minikube kubectl --"' >> ~/.bashrc
+source ~/.bashrc
+
+echo "Downloading abctl..."
+curl -LO https://github.com/airbytehq/abctl/releases/download/v0.16.0/abctl-v0.16.0-linux-amd64.tar.gz || (echo "${RED}can't download abctl" ; exit 1)
+tar -xvzf abctl-v0.16.0-linux-amd64.tar.gz
+sudo mv abctl-v0.16.0-linux-amd64/abctl /usr/local/bin/
+rm -rf abctl-v0.16.0-linux-amd64 abctl-v0.16.0-linux-amd64.tar.gz
+sudo chmod +x /usr/local/bin/abctl
+
+echo "Uninstalling any previous Airbyte installation..."
+abctl local uninstall
+
+echo "Installing Airbyte..."
+abctl local install
+
+echo "Setting up credentials for an insecure instance..."
+abctl local credentials --email user@company.example
+abctl local credentials --password new_password
+
+echo "Installation completed! Access Airbyte at http://localhost:8000"
+
+echo "Enter user@company.example/new_password as username/password."
