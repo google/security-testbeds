f2bff4e6526a856f3ead834b6c31d5234172a44c
jp-ag <jpaguinsky@gmail.com>
2024-12-13 12:51:40 +0000

    Adding kCTF challenge for bentoml

---
+++ bentoml/chal-bento/challenge/cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+logFolder="/chroot/usr/share/grafana/logs"
+
+now=$(date +%s)
+cutoff=$((now - 3*60))
+
+for file in "$logFolder"/*.log
+do
+    filename="${file##*/}"
+    file_time="${filename%.log}"
+    if [ $file_time -lt $cutoff ]; then
+     cat "$file" 2>&1 | /usr/bin/logger -t cronjob
+     rm "$file"
+    fi
+    unset filename file_time
+    sleep 1
+    cat /dev/null > /var/log/syslog
+done
+
