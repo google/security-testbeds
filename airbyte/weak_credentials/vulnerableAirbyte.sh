c906af92425209a6bfcc2dea9c479d0a58a3ffd2
secureness <nosecureness@gmail.com>
2025-05-19 17:06:16 +0400

    add debian support for docker installtion

--- airbyte/weak_credentials/vulnerableAirbyte.sh
+++ airbyte/weak_credentials/vulnerableAirbyte.sh
@@ -7,19 +7,19 @@ echo "An Automated Bash script for setting up a vulnerable airbyte instance that
 echo "Updating package index..."
 sudo apt-get update
 
-echo  "Add Docker's official GPG key..."
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+echo "Adding Docker's official GPG key..."
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/$(. /etc/os-release && echo "$ID")/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-echo  "Add the repository to Apt sources..."
+echo "Adding the repository to Apt sources..."
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+
 
 echo "Installing Docker..."
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
