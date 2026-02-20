2562e346475abe5fca575caaa04e6c9989680777
joernNNN <joernwege1@gmail.com>
2026-02-20 16:10:57 +0400

    no need to python directory here

--- apache/livy/ExposedUI/python/livy/delete_session.py
+++
@@ -1,5 +0,0 @@
-import json, pprint, requests, textwrap
-host = 'http://localhost:8998'
-
-r = requests.delete(host + '/sessions/1')
-pprint.pprint(r.json())
--- apache/livy/ExposedUI/python/livy/wait_for_idle.py
+++
@@ -1,5 +0,0 @@
-import json, pprint, requests, textwrap
-host = 'http://localhost:8998'
-
-r = requests.get(host + '/sessions/1')
-pprint.pprint(r.json())
